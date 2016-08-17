96d8dc4bf670c53001c7a7aa24141f175acce544
Nikita Sinetskiy <nsinetskiy@pravo.rg.ru>
2016-08-17 17:09:04 +0300

    Fixed jurists sorting

--- www/app/Resources/public/blocks/main/b-users-sorting/scripts/b-users-sorting.js
+++ www/app/Resources/public/blocks/main/b-users-sorting/scripts/b-users-sorting.js
@@ -1,17 +1,16 @@
 $(function() {
     function sendRequest() {
-
-        $(this).prop('disabled', 'disabled');
-
+        var this_local = callback_data['this'];
+        var select_id = '#' + $(this_local).attr('id');
         var data = {
             'current_url' : window.location.pathname,
             'current_search' : window.location.search,
         };
 
-        $('#' + $(this).attr('id') + ' option').each(function () {
-            if ($(this).prop("selected")) {
+        $('#' + $(this_local).attr('id') + ' option').each(function () {
+            if ($(this_local).prop("selected")) {
                 data['order_by'] = {};
-                data.order_by[this.parentNode.id] = $(this).val();
+                data.order_by[this_local.parentNode.id] = $(this_local).val();
             }
         });
 
@@ -19,16 +18,27 @@ $(function() {
             url: '/sort/',
             data: data,
             type: "POST",
+            async: true,
             success: function(data) {
                 console.log(data);
-                $(this).prop('disabled', false);
+                console.log(select_id);
+                $(select_id).prop('disabled', false);
             },
             error: function(data) {
                 console.log('error');
-                $(this).prop('disabled', false);
+                $(select_id).prop('disabled', false);
             }
         });
     }
 
-    $('#user-specialization, #sortBy').on('change', _.debounce(sendRequest, 1000));
+    var callback_data = {
+        'this' : ''
+    };
+
+    function callback () {
+        callback_data['this'] = $(this);
+        $($('#' + $(this).attr('id')).prop('disabled', 'disabled')).ready(_.debounce(sendRequest, 1000));
+    }
+
+    $('#user-specialization, #sortBy').on('change', callback);
 });

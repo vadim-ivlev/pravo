c3b8d89d17b4aea506b7dd07c58bdfd182bc7803
Nikita Sinetskiy <nsinetskiy@pravo.rg.ru>
2016-09-16 18:06:51 +0300

    Added bibliotechka block to the questions list

---
+++ www/app/Resources/public/blocks/crosslayouts/b-questions/b-questions.js
@@ -0,0 +1,23 @@
+module.exports = function(data){
+
+	return {
+	
+		opt: {
+			contents: [
+				{
+					param: {
+						
+						blocks: {
+
+							bibliotechkaAdvert: RGB('bibliotechka-advert, mobile tablet tabletLandscape') // блок библиотечки на мобильных устройствах
+
+						}
+						
+					}
+				}
+			]
+		}
+	
+	}
+
+};

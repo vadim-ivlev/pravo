8629d9a9fef32e2c893b60edba5356ca242ba37a
Nikita Sinetskiy <nsinetskiy@pravo.rg.ru>
2017-03-14 14:28:00 +0300

    Make new templates

---
+++ www/app/Resources/public/pages/data/tags.js
@@ -0,0 +1,152 @@
+module.exports = {
+
+    toMerge: {
+
+        meta: {
+
+            blocks: [
+
+                RGT('meta'), // метаинформация
+
+                /*
+                 * Кастомная метаинформация
+                 *
+                 */
+                {
+                    name: "crosspageOgTitle",
+                    opt: {
+                        tag: "meta",
+                        attrs: {
+                            property: "og:title",
+                            content: "Рубрики и актуальные теги — Юридическая консультация"
+                        }
+                    }
+                },
+                /*{
+                    name: "crosspagesOgDescription",
+                    opt: {
+                        tag: "meta",
+                        attrs: {
+                            property: "og:description",
+                            content: "Правовая поддержка граждан: вы можете получить юридическую помощь бесплатно. Задайте вопрос опытному юристу"
+                        }
+                    }
+                },*/
+                {
+                    name: "indexTitle",
+                    opt: {
+                        tag: "title",
+                        contents: [
+                            {
+                                data: "Рубрики и актуальные теги — Юридическая консультация"
+                            }
+                        ]
+                    }
+                },
+                /*{
+                    name: "indexDescription",
+                    opt: {
+                        tag: "meta",
+                        attrs: {
+                            name: "description",
+                            content: "Российская газета - издание Правительства Российской Федерации, официальный публикатор документов"
+                        }
+                    }
+                },*/
+                {
+                    name: "indexYandexVerification",
+                    opt: {
+                        tag: "meta",
+                        attrs: {
+                            name: "yandex-verification",
+                            content: "68d57ae756ac44ad"
+                        }
+                    }
+                },
+                {
+                    name: "rgAds",
+                    opt: {
+                        contents: [
+                            { data: "<meta name=\"rg-data\" property=\"ads:uri\" content=\"/pravo/rubrics/\">"}, // Для подключения рекламы
+                        ]
+                    }
+                }
+
+            ]
+
+        },
+
+        /*
+         * Шапка
+         *
+         */
+
+        head: {
+
+            blocks: [
+
+                //RGInclude('/custom/projects/juristical/global', 'styles'), // стили
+
+                //RGInclude('/custom/projects/juristical/global', 'scripts') // скрипты
+
+            ]
+        },
+
+
+        /*
+         * Основной блок контента
+         *
+         */
+
+        main: {
+
+            blocks: [
+
+                RGB('header'), // шапка сайта
+
+                RGB('main:tags') // основной контент
+
+            ]
+
+        },
+
+        /*
+         * Левый сайдбар
+         *
+         */
+
+        sidebarLeft: {
+
+            blocks: [
+
+                RGB('sidebar-left:tags', '#sidebar')
+
+            ]
+        },
+
+        /*
+         * Правый сайдбар
+         *
+         */
+
+        sidebarRight: {
+
+            blocks: [
+
+                RGB('sidebar-right:tags', '#sidebar')
+
+            ]
+        },
+
+
+        /*
+         * Метаинформация
+         *
+         */
+
+        _meta: {
+            uri: "juristical"
+        }
+    }
+
+};

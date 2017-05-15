b8a0e12b0abad1d2431b68691d3f1c73f033847a
web coder Glushinkov Yurii <glushenkov@pravo.rg.ru>
2017-05-15 10:56:33 +0300

    add to gitignore www/web/sitemaps/* AND hide app_dev.php with return 404

--- www/web/app_dev.php
+++ www/web/app_dev.php
@@ -11,8 +11,14 @@ use Symfony\Component\Debug\Debug;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 
-if(@$_SERVER['REMOTE_ADDR'] !== '212.69.111.131' && @$_SERVER['REMOTE_ADDR'] !== '192.168.1.2') {
+function closeAppDev() {
+    header("HTTP/1.x 404 Not Found");
+    include("/var/www/pravo/www/app/Resources/TwigBundle/views/Exception/error404.html");
     die;
+}
+
+if(@$_SERVER['REMOTE_ADDR'] !== '212.69.111.131' && @$_SERVER['REMOTE_ADDR'] !== '192.168.1.2') {
+    closeAppDev();
 }
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])

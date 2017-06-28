f7a48db8dc3643e27ae70be2647c3cba2e6a82ec
Nikita Sinetskiy <nsinetskiy@pravo.rg.ru>
2017-06-28 18:08:29 +0300

    Duplicate question form link to jurist profile, fix layout of question blocks and categories select on mobiles

--- www/app/Resources/public/blocks/crosslayouts/b-jurists/b-jurists.js
+++ www/app/Resources/public/blocks/crosslayouts/b-jurists/b-jurists.js
@@ -2,15 +2,13 @@ module.exports = function(data){
 
     return {
 
-        name: "bMenu",
+        name: "bJurists",
         opt: {
             contents: [
                 {
                     param: {
 
                         blocks: {
-
-                            tabs: RGB('tabs:regular, tablet tabletLandscape desktop desktopFull:active'), // табы
 
                             askLink: RGB('ask-link') // ссылка на форму "Задать вопрос"
 
